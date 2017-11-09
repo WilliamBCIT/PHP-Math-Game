d354654bd05a8442a9d17e6a70bb049812acec85
William Bradshaw <wbradshaw4@learn.bcit.ca>
2017-11-09 15:37:00 -0800

    Added HTML5 templates to PHP pages

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8" />
+   <title>Math Game - Play</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" media="screen">
+   <link href="css/MathGame.css" rel="stylesheet" media="screen">
+   <style>
+      div[class^="col-"] {
+      height: 100px; text-align: center; border: 2px solid black;
+   }
+   </style>
+</head>
+<body>
+   <div class="container">
+   <div class="row bg-warning">
+   <div class="col-xs-12">col-xs-12</div>
+   </div>
+   <div class="row">
+   </div>
+</div>
+</body>
+</html>
