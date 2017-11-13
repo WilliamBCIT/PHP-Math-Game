1f8fdc1ef1fed720a8c2e0de827a5119ba3630fa
William Bradshaw <wbradshaw4@learn.bcit.ca>
2017-11-12 21:13:33 -0800

    Added Set A + Encoded Emails

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
 <div class="container margin-top-10">
     <div class="page-footer text-center">
-        <h3 class="footer-text vertical-align"> Website designed by William Bradshaw(A01020058) and Vladimir Kisselev(A00798129)</p>
+        <h3 class="footer-text vertical-align">Set A - Website Project designed by <a href='&#109;ail&#116;o&#58;w&#98;r%61%64sha%774&#64;&#108;%&#54;&#53;ar%6&#69;&#46;bcit&#46;%&#54;3a'>William Bradshaw</a>(A01020058) and <a href='mailt&#111;&#58;V%76&#37;6C&#97;&#100;&#105;mir%40&#109;y%&#50;Eb%&#54;3%69t&#46;&#99;%61'>Vladimir Kisselev</a>(A00798129)</p>
     </div>
 </div>
 </body>
